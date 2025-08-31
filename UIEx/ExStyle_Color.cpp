b912e2ad472cab9511167c628464f6b692d21ed0
Tobias Bohnen <1294616+TobiasBohnen@users.noreply.github.com>
2025-08-31 19:49:28 +0200

    api update

--- UIEx/ExStyle_Color.cpp
+++ UIEx/ExStyle_Color.cpp
@@ -512,11 +512,11 @@ auto create_color_styles(assets::group const& resGrp) -> style_collection
         style->Margin           = {5_px};
         style->Padding          = {5_px};
         style->DropShadow.Color = color {0, 0, 0, 128};
-        style->HeaderSize       = 10_pct;
+        style->TabBarSize       = 10_pct;
         style->TabItemClass     = "tab_items";
-        style->HeaderPosition   = position::Top;
-        style->HeaderMode       = tab_container::header_mode::Fill;
-        style->HeaderLineCount  = 3;
+        style->TabBarPosition   = position::Top;
+        style->TabBarMode       = tab_container::header_mode::Fill;
+        style->TabBarRows       = 3;
 
         auto hoverStyle {retValue.create<tab_container>("tab_container", {.Hover = true})};
         *hoverStyle = *style;
--- UIEx/ExStyle_Skin.cpp
+++ UIEx/ExStyle_Skin.cpp
@@ -310,10 +310,10 @@ auto create_skinned_styles(assets::group const& resGrp) -> style_collection
         style->Margin           = {5_px};
         style->Padding          = {5_px};
         style->DropShadow.Color = color {0, 0, 0, 128};
-        style->HeaderSize       = 10_pct;
+        style->TabBarSize       = 10_pct;
         style->TabItemClass     = "tab_items";
-        style->HeaderPosition   = position::Bottom;
-        style->HeaderMode       = tab_container::header_mode::Compact;
+        style->TabBarPosition   = position::Bottom;
+        style->TabBarMode       = tab_container::header_mode::Compact;
 
         auto disabledStyle {retValue.create<tab_container>("tab_container", {.Disabled = true})};
         disabledStyle->Background = colors::FireBrick;
