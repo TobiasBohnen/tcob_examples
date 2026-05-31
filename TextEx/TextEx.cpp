d63aa7e355ac6d5ab82456ee2527561560211155
Tobias Bohnen <1294616+TobiasBohnen@users.noreply.github.com>
2026-05-31 11:28:50 +0200

    update

--- TextEx/TextEx.cpp
+++ TextEx/TextEx.cpp
@@ -18,20 +18,20 @@ void TextEx::on_start()
 {
     auto* resGrp {library().get_group("text")};
     auto  fontFam {resGrp->get<font_family>("Poppins")};
-    auto  font {fontFam->get_font({}, 32)};
 
     seconds duration {4s};
 
     {
-        auto& text0 {_texts.emplace_back(std::make_unique<text>(font))};
+        auto& text0 {_texts.emplace_back(std::make_unique<text>(fontFam))};
         text0->Text   = "normal text \n"
                         "{COLOR:Red}c{COLOR:Blue}o{COLOR:Yellow}l{COLOR:LightBlue}o{COLOR:Cyan}r{COLOR:Orange}e{COLOR:Blue}d {COLOR:RebeccaPurple}t{COLOR:Green}e{COLOR:Indigo}x{COLOR:LimeGreen}t{COLOR:White}\n"
                         "{ALPHA:1}t{ALPHA:0.9}r{ALPHA:0.8}a{ALPHA:0.7}n{ALPHA:0.6}s{ALPHA:0.5}p{ALPHA:0.4}a{ALPHA:0.3}r{ALPHA:0.2}e{ALPHA:0.1}n{ALPHA:1}t te{ALPHA:0.5}xt";
+        text0->Style  = text::style {.Color = colors::White, .Alignment = {}, .KerningEnabled = false, .FontStyle = {}, .FontSize = 32};
         text0->Bounds = {{30, 6}, {500, 500}};
     }
 
     {
-        auto& text0 {_texts.emplace_back(std::make_unique<text>(font))};
+        auto& text0 {_texts.emplace_back(std::make_unique<text>(fontFam))};
         text0->Text   = "{EFFECT:1}The quick brown fox{EFFECT:0} jumps over {EFFECT:2}the lazy dog{EFFECT:0}.\n"
                         "{EFFECT:3}PACK MY BOX{EFFECT:0} with {EFFECT:4}five dozen liquor jugs{EFFECT:0}!\n"
                         "{EFFECT:5}Sphinx of black quartz{EFFECT:0}, judge my vow.\n"
@@ -40,8 +40,8 @@ void TextEx::on_start()
                         "{EFFECT:9}The five boxing wizards jump quickly{EFFECT:0}.\n"
                         "{EFFECT:10}Bright vixens jump{EFFECT:0}; dozy fowl quack.\n"
                         "{EFFECT:11}Waltz, nymph{EFFECT:0}, for quick jigs vex Bud.\n\n";
-        text0->Style  = text::style {.Color = colors::White, .Alignment = {}, .KerningEnabled = false};
-        text0->Bounds = {{420, 6}, {900, 1000}};
+        text0->Style  = text::style {.Color = colors::White, .Alignment = {}, .KerningEnabled = false, .FontStyle = {}, .FontSize = 40};
+        text0->Bounds = {{420, 6}, {900, 1200}};
         auto& effects {text0->Effects};
         effects.create(1, duration, effect::typing {});
         effects.create(2, duration, effect::fade_in {3});
@@ -58,11 +58,12 @@ void TextEx::on_start()
     }
 
     {
-        auto& text0 {_texts.emplace_back(std::make_unique<text>(font))};
+        auto& text0 {_texts.emplace_back(std::make_unique<text>(fontFam))};
         text0->Text   = "{EFFECT:1}Earthquake detected...{EFFECT:0}\n"
                         "{EFFECT:2}Ocean waves rolling in{EFFECT:0}\n\n"
                         "{EFFECT:3}{COLOR:Purple}Balloon inflating{EFFECT:0}\n\n"
                         "{EFFECT:4}{COLOR:Green}Spinning tornado{EFFECT:0}\n";
+        text0->Style  = text::style {.Color = colors::White, .Alignment = {}, .KerningEnabled = false, .FontStyle = {}, .FontSize = 32};
         text0->Bounds = {{30, 206}, {500, 1000}};
         auto& effects {text0->Effects};
         effects.create(1, duration, effect::typing {}, effect::shake {.Intensity = 3.f, .RNG = rng {12345}})->Interval = 100ms;
